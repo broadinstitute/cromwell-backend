1845ef33f61746fa62ff7c69862774fbf586543c
Gaurav <gaurav1.k.singh@intel.com>
2016-02-12 13:03:24 -0800

    Recovering logic plus a small test for it

--- src/main/scala/cromwell/backend/BackendValidationActor.scala
+++ src/main/scala/cromwell/backend/BackendValidationActor.scala
@@ -2,24 +2,23 @@ package cromwell.backend
 
 import akka.actor.{Actor, ActorLogging}
 import akka.event.LoggingReceive
-import cromwell.backend.BackendValidationActor.{Validate, ValidationResult}
-import wdl4s.{WorkflowCoercedInputs, NamespaceWithWorkflow}
+import cromwell.backend.BackendValidationActor.{FailedValidationResult, Validate, ValidationResult}
+import wdl4s.{NamespaceWithWorkflow, WorkflowCoercedInputs}
 
 import scala.concurrent.Future
 
 object BackendValidationActor {
   sealed trait BackendValidationActorMessage
-  case class Validate(namespace: NamespaceWithWorkflow, wfInputs: Option[WorkflowCoercedInputs] = None, wfOptionsJson: Option[String] = None) extends BackendValidationActorMessage
-
   sealed trait ValidationResult extends BackendValidationActorMessage
-  case object SuccessfulValidationResult extends ValidationResult
+  case class Validate(namespace: NamespaceWithWorkflow, wfInputs: Option[WorkflowCoercedInputs] = None, wfOptionsJson: Option[String] = None) extends BackendValidationActorMessage
   final case class FailedValidationResult(errors: List[String]) extends ValidationResult
+  case object SuccessfulValidationResult extends ValidationResult
 }
 
 /**
   * Note: This receives a whole workflow as opposed to task at a time methodology which the other parts of this component stick to.
-  *       It should be made uniform about what it is that we do, i.e. WF at a time or Task at a time. This is currently to support
-  *       validations in the JES Backend.
+  * It should be made uniform about what it is that we do, i.e. WF at a time or Task at a time. This is currently to support
+  * validations in the JES Backend.
   *
   * It should be assumed that the basic WF validation, such as input coercion or namespace validation has already been performed
   *
@@ -34,8 +33,8 @@ trait BackendValidationActor extends Actor with ActorLogging {
 
   /**
     *
-    * @param namespace Represent a directly runnable WDL Namespace
-    * @param wfInputs Workflow options specified as a Json String
+    * @param namespace     Represent a directly runnable WDL Namespace
+    * @param wfInputs      Workflow options specified as a Json String
     * @param wfOptionsJson Workflow options specified as a Json String
     * @return True (wrapped in a message) to indicate a Yay!
     */
@@ -47,6 +46,10 @@ trait BackendValidationActor extends Actor with ActorLogging {
       val requester = sender()
       validateWorkflow(namespace, inputsJson, optionsJson) map {
         requester ! _
+      } recover {
+        // The call to validateWorkflow resulted in an exception
+        case exception: Throwable =>
+          requester ! FailedValidationResult(List(s"Failed to validate the workflow: ${exception.getMessage}"))
       }
     case unknownMessage => log.error(s"BackendValidationActor received an unknown message: ${unknownMessage}")
   }
---
+++ src/test/scala/cromwell/backend/BackendValidationActorSpec.scala
@@ -0,0 +1,66 @@
+package cromwell.backend
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit}
+import cromwell.backend.BackendValidationActor.{FailedValidationResult, SuccessfulValidationResult, Validate, ValidationResult}
+import org.scalatest.{BeforeAndAfterAll, MustMatchers, WordSpecLike}
+import wdl4s._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class BackendValidationActorSpec extends TestKit(ActorSystem("BackendValidationActorSpec"))
+  with WordSpecLike
+  with MustMatchers
+  with BeforeAndAfterAll
+  with ImplicitSender {
+
+  val helloWorldWdl =
+    """
+      |task hello {
+      |  command {
+      |    echo "Hello World!"
+      |  }
+      |  output {
+      |    String salutation = read_string(stdout())
+      |  }
+      |  runtime {
+      |   docker: "some docker"
+      |  }
+      |}
+      |
+      |workflow hello {
+      |  call hello
+      |}
+    """.stripMargin
+
+  class SomeConcreteBackend(shouldThrow: Boolean = false) extends BackendValidationActor {
+    override def validateWorkflow(namespace: NamespaceWithWorkflow, wfInputs: Option[WorkflowCoercedInputs] = None, wfOptionsJson: Option[String] = None): Future[ValidationResult] = Future {
+      shouldThrow match {
+        case false => SuccessfulValidationResult
+        case true => throw new IllegalStateException("Some exception...")
+      }
+    }
+  }
+
+  "BackendValidationActor" must {
+    "return a SuccessfulValidationResult" in {
+      val backend = system.actorOf(Props(new SomeConcreteBackend()))
+      //new SomeConcreteBackend("Yes it fails!", true)
+      backend ! Validate(NamespaceWithWorkflow.load(helloWorldWdl))
+      expectMsg(SuccessfulValidationResult)
+      system.stop(backend)
+    }
+    "return a FailedValidationResult in case the call to validateWorkflow throws Exception" in {
+      val backend = system.actorOf(Props(new SomeConcreteBackend(true)))
+      //new SomeConcreteBackend("Yes it fails!", true)
+      backend ! Validate(NamespaceWithWorkflow.load(helloWorldWdl))
+      expectMsgClass(classOf[FailedValidationResult])
+      system.stop(backend)
+    }
+  }
+
+  override def afterAll(): Unit = system.shutdown()
+
+
+}
